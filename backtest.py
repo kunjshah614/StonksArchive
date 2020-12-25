abab047f9e864d2a8e79046eb25c9f6ed0053227
kunjshah614 <kunjshah614@gmail.com>
2020-12-25 11:36:48 -0500

    Continued updating backtesting function

--- backtest.py
+++ backtest.py
@@ -1,9 +1,8 @@
 #Script to backtest trades
 
-#Update record fcn with ability to update "stocks" sheet (qty)
-#Update checkQty fcn
 #Update checkDayTrades fcn
 #Update buy fcn to update "stocks" sheet (most recent buy)
+#Add function to return the amount of a given stock (used outside of "backtest.py"
 
 from yahoo_fin import stock_info as si
 from datetime import datetime as dt
@@ -24,6 +23,19 @@ def record(time, ticker, price, quantity): #if all checks pass, record the trade
     sh['C' + str(currRow)] = price
     sh['D' + str(currRow)] = quantity
     sh['E' + str(currRow)] = np.round(sh['E' + str(prevrow)].value - sh['C' + str(currRow)].value*quantity, 2)
+    
+    sh = wb['Stocks'] #add stock to spreadsheet
+    nameFound = False
+
+    for cell in sh['A']:
+        if(cell.value == ticker):
+            if quantity < 0:
+                sh['B' + str(cell.row)] = time
+            sh['C' + str(cell.row)] = sh['C' + str(cell.row)].value + quantity
+            nameFound = True
+    if nameFound == False:
+        currRow = sh.max_row + 1
+        sh['A' + str(currRow)] = ticker
     
     wb.save(filename = 'log.xlsx')
 
@@ -53,8 +65,28 @@ def checkBalance(price, quantity): #check balance
         wb.save(filename = 'log.xlsx')
         return False
     
-def checkQty(ticker): #FINISH THIS
-    return True
+def checkQty(ticker, quantity): #PREVENTING TRADE FROM GOING THROUGH
+    wb = op.load_workbook(filename = 'log.xlsx')
+    sh = wb['Stocks'] #add stock to spreadsheet
+    nameFound = False
+
+    for cell in sh['A']:
+        if(cell.value == ticker):
+            if sh['C' + str(cell.row)].value >= quantity:
+                return True
+            else:
+                print('Insufficient Stock Quantity')
+                sh = wb['Errors']
+                sh['A3'] = 'YES'
+                wb.save(filename = 'log.xlsx')
+                return False
+            nameFound = True
+    if nameFound == False:
+        print('Insufficient Stock Quantity')
+        sh = wb['Errors']
+        sh['A3'] = 'YES'
+        wb.save(filename = 'log.xlsx')
+        return False
 
 def checkDayTrades(ticker): #FINISH THIS
     return True
@@ -67,7 +99,7 @@ def buy(ticker, quantity):
     
     if checkHours(t): #check trading hours
         if checkBalance(price, quantity): #check balance
-            record(t, ticker, price, quantity)
+            record(t, ticker, price, quantity) #record the trade
             
 def sell(ticker, quantity):
     #check trading hours, quantity of stocks to be sold, day trades
@@ -76,8 +108,8 @@ def sell(ticker, quantity):
     price = si.get_live_price(ticker)
     
     if checkHours(t): #check trading hours
-        if checkQty(ticker): #check quantity of stocks
+        if checkQty(ticker, quantity): #check quantity of stocks
             if checkDayTrades(ticker): #check day trades
-                record(t, ticker, price, -quantity)
+                record(t, ticker, price, -quantity) #record the trade
 
-sell('NIO', 20)
+sell('NIO', 45)
