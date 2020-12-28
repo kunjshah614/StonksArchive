51b67004e022d1f541897b6e39cfd7f73c1eeb27
kunjshah614 <kunjshah614@gmail.com>
2020-12-28 14:21:05 -0500

    correlation

--- getStockData.py
+++ getStockData.py
@@ -1,58 +1,61 @@
-from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time as tm
 
-def getWeighting (ticker, inter):
-    class returnVal:
+apiKey = '63SKE6NA9MIIRXO5'
+# apiKey = 'TZPVUS2QURXT3WD5'
+# apiKey = 'Q207WCDSVFKS8KOG'
+ti = TechIndicators(key=apiKey,output_format='pandas')
+tperiod = 20
+# ind = ['EMA', 'MACD', 'RSI', 'CCI', 'stoch', 'ADX', 'aroon', 'AD', 'OBV']
+
+class returnVal:
         #Used to return multiple variables from leastSquares fcn
-        def __init__(self, m, b, error, cov):
-            self.m = m
-            self.b = b
-            self.error = error
-            self.cov = cov
+    def __init__(self, m, b, error, corr):
+        self.m = m
+        self.b = b
+        self.error = error
+        self.corr = corr
             
-    def leastSquares(xVals, yVals, indicator, plots):
-        #Ax = b where A = [A1 A2] and A = QR
-        A1 = np.transpose(np.asmatrix(xVals))
-        A2 = np.transpose(np.asmatrix(np.ones(np.size(A1))))
-        A = np.concatenate((A1, A2), axis = 1) #Formation of A matrix
-        Q, R = np.linalg.qr(A, mode = 'complete') #QR factorization to avoid a computationally expensive inverse
-        bmat = np.transpose(np.asmatrix(yVals)) #Formation of b vector
-        x = np.matmul(np.linalg.pinv(R),np.transpose(Q)*bmat) #Computation of m and b in y = mx + b
-        m = x[0, 0]
-        b = x[1, 0]
-        xpts = [np.min(A1), np.max(A1)] #x pts for best fit line
-        ypts = [m*xpts[0] + b, m*xpts[1] + b] #y pts for best fit line
-        error = np.sqrt(np.sum(np.square(b - (m*A1 + b*A2)))) #Computation of euclidian norm
-        
-        cov = np.sum((xVals - np.mean(xVals))*(yVals - np.mean(yVals)))/np.size(yVals) #this doesn't seem to be right...
+def leastSquares(xVals, yVals, indicator, plots):
+    #Ax = b where A = [A1 A2] and A = QR
+    A1 = np.transpose(np.asmatrix(xVals))
+    A2 = np.transpose(np.asmatrix(np.ones(np.size(A1))))
+    A = np.concatenate((A1, A2), axis = 1) #Formation of A matrix
+    Q, R = np.linalg.qr(A, mode = 'complete') #QR factorization to avoid a computationally expensive inverse
+    bmat = np.transpose(np.asmatrix(yVals)) #Formation of b vector
+    x = np.matmul(np.linalg.pinv(R),np.transpose(Q)*bmat) #Computation of m and b in y = mx + b
+    m = x[0, 0]
+    b = x[1, 0]
+    xpts = [np.min(A1), np.max(A1)] #x pts for best fit line
+    ypts = [m*xpts[0] + b, m*xpts[1] + b] #y pts for best fit line
+    error = np.sqrt(np.sum(np.square(b - (m*A1 + b*A2)))) #Computation of euclidian norm
+    
+    cov = np.sum((xVals - np.mean(xVals))*(yVals - np.mean(yVals)))/np.size(yVals)
+    varX = np.sum(np.square(xVals - np.mean(xVals)))/np.size(xVals)
+    varY = np.sum(np.square(yVals - np.mean(yVals)))/np.size(yVals)
+    corr = cov/(np.sqrt(varY)*np.sqrt(varX))
 
-        if plots == True:
-            # Make the plots
-            plt.figure(plt.gcf().number + 1)
-            plt.gcf().canvas.set_window_title(indicator)
-            plt.title('Price Change vs ' + indicator)
-            plt.xlabel(indicator)
-            plt.ylabel('Price Change ($)')
-            plt.figtext(1, 0, "Error: " + str(np.round(error, 2)), horizontalalignment = 'right', fontweight = 'extra bold')
-            plt.figtext(1, 0.03, "Covariance: " + str(np.round(cov, 2)), horizontalalignment = 'right', fontweight = 'extra bold')
-            plt.scatter(np.asarray(A1), np.asarray(bmat), label = 'Data')
-            plt.plot(xpts, ypts, 'r', label = 'Best Fit')
-            plt.legend()        
+    if plots == True:
+        # Make the plots
+        plt.figure(plt.gcf().number + 1)
+        plt.gcf().canvas.set_window_title(indicator)
+        plt.title('Price Change vs ' + indicator)
+        plt.xlabel(indicator)
+        plt.ylabel('Price Change ($)')
+        plt.figtext(1, 0, "Error: " + str(np.round(error, 2)), horizontalalignment = 'right', fontweight = 'extra bold')
+        plt.figtext(1, 0.03, "Correlation: " + str(np.round(corr, 2)), horizontalalignment = 'right', fontweight = 'extra bold')
+        plt.scatter(np.asarray(A1), np.asarray(bmat), label = 'Data')
+        plt.plot(xpts, ypts, 'r', label = 'Best Fit')
+        plt.legend()        
 
-        return returnVal(m, b, error, cov)
+    return returnVal(m, b, error, corr)
 
-    apiKey = 'TZPVUS2QURXT3WD5'
-
-    ts = TimeSeries(key=apiKey,output_format='pandas')
-    ti = TechIndicators(key=apiKey,output_format='pandas')
-
-    tperiod = 20
-
+def getWeighting (ticker, inter):
     # Get data
+    print('1')
     EMAData, _ = ti.get_ema(symbol = ticker, interval = inter, time_period = tperiod, series_type = 'close')
     MACDData, _ = ti.get_macd(symbol = ticker, interval = inter, series_type = 'close')
     RSIData, _ = ti.get_rsi(symbol = ticker, interval = inter, time_period = tperiod, series_type = 'close')
@@ -64,23 +67,23 @@ def getWeighting (ticker, inter):
     ADData, _ = ti.get_ad(symbol = ticker, interval = inter)
     OBVData, _ = ti.get_obv(symbol = ticker, interval = inter)
 
+    print('2')
     # Extract series from dataframe
     EMAVals = EMAData.loc[:,'EMA']
     MACDVals = MACDData.loc[:,'MACD']
     RSIVals = RSIData.loc[:,'RSI']
     CCIVals = CCIData.loc[:,'CCI']
-    stochVals = stochData.loc[:, 'SlowD']
-    stochVals.rename('stoch', inplace = True)
+    stochVals = stochData.loc[:, 'SlowD'].rename('stoch', inplace = True)
     ADXVals = ADXData.loc[:,'ADX']
-    aroonVals = aroonData.loc[:,'Aroon Up'] - aroonData.loc[:,'Aroon Down']
-    aroonVals.rename('aroon', inplace = True)
-    ADVals = ADData.loc[:, 'Chaikin A/D']
-    ADVals.rename('AD', inplace = True)
+    aroonVals = (aroonData.loc[:,'Aroon Up'] - aroonData.loc[:,'Aroon Down']).rename('aroon', inplace = True)
+    ADVals = ADData.loc[:, 'Chaikin A/D'].rename('AD', inplace = True)
     OBVVals = OBVData.loc[:, 'OBV']
 
+    print('3')
     # Create dP/dt data
     closeDiff = -EMAVals.diff(periods = -1)
 
+    print('4')
     # Line up all the data
     MACDTotal = pd.concat([closeDiff, MACDVals], axis = 1)
     MACDTotal.dropna(inplace = True)
@@ -99,6 +102,7 @@ def getWeighting (ticker, inter):
     OBVTotal = pd.concat([closeDiff, OBVVals], axis = 1)
     OBVTotal.dropna(inplace = True)
 
+    print('5')
     # Least Squares (individual)
     plots = False
     MACD = leastSquares(MACDTotal['MACD'].to_numpy(), MACDTotal['EMA'].to_numpy(), 'MACD', plots)
@@ -110,28 +114,24 @@ def getWeighting (ticker, inter):
     AD = leastSquares(ADTotal['AD'].to_numpy(), ADTotal['EMA'].to_numpy(), 'AD', plots)
     OBV = leastSquares(OBVTotal['OBV'].to_numpy(), OBVTotal['EMA'].to_numpy(), 'OBV', plots)
 
+    print('6')
     # Linear Combination
-    w = np.array([1/MACD.cov, 1/RSI.cov, 1/CCI.cov, 1/stoch.cov, 1/ADX.cov, 1/aroon.cov, 1/AD.cov, 1/OBV.cov])
+    w = np.array([MACD.corr, RSI.corr, CCI.corr, stoch.corr, ADX.corr, aroon.corr, AD.corr, OBV.corr])
     w = w/w.sum()
     total = pd.DataFrame({'Indicator': ['MACD', 'RSI', 'CCI', 'stoch', 'ADX', 'aroon', 'AD', 'OBV'],
                           'w': w,
                           'm': [MACD.m, RSI.m, CCI.m, stoch.m, ADX.m, aroon.m, AD.m, OBV.m],
                           'b': [MACD.b, RSI.b, CCI.b, stoch.b, ADX.b, aroon.b, AD.b, OBV.b]})
-
+    
+    print('7')
     plt.close(1)
     plt.show()
     
     return total
 
 def getCurrent(ticker, inter):
-    apiKey = 'TZPVUS2QURXT3WD5'
-
-    ts = TimeSeries(key=apiKey,output_format='pandas')
-    ti = TechIndicators(key=apiKey,output_format='pandas')
-
-    tperiod = 20
-
     # Get data
+    print('8')
     EMAData, _ = ti.get_ema(symbol = ticker, interval = inter, time_period = tperiod, series_type = 'close')
     MACDData, _ = ti.get_macd(symbol = ticker, interval = inter, series_type = 'close')
     RSIData, _ = ti.get_rsi(symbol = ticker, interval = inter, time_period = tperiod, series_type = 'close')
@@ -143,19 +143,18 @@ def getCurrent(ticker, inter):
     ADData, _ = ti.get_ad(symbol = ticker, interval = inter)
     OBVData, _ = ti.get_obv(symbol = ticker, interval = inter)
     
+    print('9')
     EMAVals = EMAData.loc[:,'EMA']
     MACDVals = MACDData.loc[:,'MACD']
     RSIVals = RSIData.loc[:,'RSI']
     CCIVals = CCIData.loc[:,'CCI']
-    stochVals = stochData.loc[:, 'SlowD']
-    stochVals.rename('stoch', inplace = True)
+    stochVals = stochData.loc[:, 'SlowD'].rename('stoch', inplace = True)
     ADXVals = ADXData.loc[:,'ADX']
-    aroonVals = aroonData.loc[:,'Aroon Up'] - aroonData.loc[:,'Aroon Down']
-    aroonVals.rename('aroon', inplace = True)
-    ADVals = ADData.loc[:, 'Chaikin A/D']
-    ADVals.rename('AD', inplace = True)
+    aroonVals = (aroonData.loc[:,'Aroon Up'] - aroonData.loc[:,'Aroon Down']).rename('aroon', inplace = True)
+    ADVals = ADData.loc[:, 'Chaikin A/D'].rename('AD', inplace = True)
     OBVVals = OBVData.loc[:, 'OBV']
     
+    print('10')
     info = np.array([MACDVals.iloc[-1], RSIVals.iloc[-1], CCIVals.iloc[-1], stochVals.iloc[-1], ADXVals.iloc[-1], aroonVals.iloc[-1], ADVals.iloc[-1], OBVVals.iloc[-1]])
     
 #     info = pd.DataFrame({'Indicator': ['MACD', 'RSI', 'CCI', 'stoch', 'ADX', 'aroon', 'AD', 'OBV'],
--- score.py
+++ score.py
@@ -1,94 +1,62 @@
-import leastSquares as ls
+import getStockData as sd
 import time as tm
 import numpy as np
 import pandas as pd
 import openpyxl as op
 
+mData = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] #columns containing each type of data
+bData = ['J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q']
+wData = ['R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
 
-def learn(tickerList):
-    wb = op.load_workbook(filename = 'stockData.xlsx')
-    sh = wb['stockData']
+def populate(ticker, stockConstants, rowNumber, sh):
+    for j in mData:
+        sh[j + str(rowNumber)] = stockConstants.loc[mData.index(j),'m']
+    for k in bData:
+        sh[k + str(rowNumber)] = stockConstants.loc[bData.index(k),'b']
+    for l in wData:
+        sh[l + str(rowNumber)] = stockConstants.loc[wData.index(l),'w']
 
-    for i in tickerList:
+def learn(tickerList, inter):
+    wb = op.load_workbook(filename = 'stockData.xlsx') #open the workbook
+    sh = wb[inter] #open the worksheet
+
+    for i in tickerList: #for each of the tickers...
         print(i)
-        
-        vals = ls.getWeighting(i, 'daily')
+        print('(Obtaining and Learning from Historical Data from AlphaVantage)')
+        stockConstants = sd.getWeighting(i, inter) #get m (slope), b (intercept), and w (weighting)
         tm.sleep(60)
         
-        nameFound = False
+        nameFound = False #used to determine if the stock already exists
         
-        for cell in sh['A']:     
-            if(cell.value == i): #if the stock already exists
-                sh['B' + str(cell.row)] = vals.loc[0,'m']
-                sh['C' + str(cell.row)] = vals.loc[1,'m']
-                sh['D' + str(cell.row)] = vals.loc[2,'m']
-                sh['E' + str(cell.row)] = vals.loc[3,'m']
-                sh['F' + str(cell.row)] = vals.loc[4,'m']
-                sh['G' + str(cell.row)] = vals.loc[5,'m']
-                sh['H' + str(cell.row)] = vals.loc[6,'m']
-                sh['I' + str(cell.row)] = vals.loc[7,'m']
-                sh['J' + str(cell.row)] = vals.loc[0,'b']
-                sh['K' + str(cell.row)] = vals.loc[1,'b']
-                sh['L' + str(cell.row)] = vals.loc[2,'b']
-                sh['M' + str(cell.row)] = vals.loc[3,'b']
-                sh['N' + str(cell.row)] = vals.loc[4,'b']
-                sh['O' + str(cell.row)] = vals.loc[5,'b']
-                sh['P' + str(cell.row)] = vals.loc[6,'b']
-                sh['Q' + str(cell.row)] = vals.loc[7,'b']
-                sh['R' + str(cell.row)] = vals.loc[0,'w']
-                sh['S' + str(cell.row)] = vals.loc[1,'w']
-                sh['T' + str(cell.row)] = vals.loc[2,'w']
-                sh['U' + str(cell.row)] = vals.loc[3,'w']
-                sh['V' + str(cell.row)] = vals.loc[4,'w']
-                sh['W' + str(cell.row)] = vals.loc[5,'w']
-                sh['X' + str(cell.row)] = vals.loc[6,'w']
-                sh['Y' + str(cell.row)] = vals.loc[7,'w']
-                nameFound = True
-        if nameFound == False: #if the stock does not exist (has never been bought before)
-            currRow = sh.max_row + 1
-            sh['A' + str(currRow)] = i
-            sh['B' + str(currRow)] = vals.loc[0,'m']
-            sh['C' + str(currRow)] = vals.loc[1,'m']
-            sh['D' + str(currRow)] = vals.loc[2,'m']
-            sh['E' + str(currRow)] = vals.loc[3,'m']
-            sh['F' + str(currRow)] = vals.loc[4,'m']
-            sh['G' + str(currRow)] = vals.loc[5,'m']
-            sh['H' + str(currRow)] = vals.loc[6,'m']
-            sh['I' + str(currRow)] = vals.loc[7,'m']
-            sh['J' + str(currRow)] = vals.loc[0,'b']
-            sh['K' + str(currRow)] = vals.loc[1,'b']
-            sh['L' + str(currRow)] = vals.loc[2,'b']
-            sh['M' + str(currRow)] = vals.loc[3,'b']
-            sh['N' + str(currRow)] = vals.loc[4,'b']
-            sh['O' + str(currRow)] = vals.loc[5,'b']
-            sh['P' + str(currRow)] = vals.loc[6,'b']
-            sh['Q' + str(currRow)] = vals.loc[7,'b']
-            sh['R' + str(currRow)] = vals.loc[0,'w']
-            sh['S' + str(currRow)] = vals.loc[1,'w']
-            sh['T' + str(currRow)] = vals.loc[2,'w']
-            sh['U' + str(currRow)] = vals.loc[3,'w']
-            sh['V' + str(currRow)] = vals.loc[4,'w']
-            sh['W' + str(currRow)] = vals.loc[5,'w']
-            sh['X' + str(currRow)] = vals.loc[6,'w']
-            sh['Y' + str(currRow)] = vals.loc[7,'w']
+        for cell in sh['A']: #loop through every cell in column 'A'    
+            if(cell.value == i): #if the stock already exists...
+                rowNumber = cell.row #the row number is the row with the ticker in it
+                nameFound = True 
+        if nameFound == False: #if the stock does not exist (has never been bought before)...
+            rowNumber = sh.max_row + 1 #the row number is the next empty row
+            sh['A' + str(rowNumber)] = i #enter the ticker name in column 'A'
+        print('(Writing Data to Spreadsheet)\n')
+        populate(i, stockConstants, rowNumber, sh) #populate the row with the data
         
-    wb.save(filename = 'stockData.xlsx')
+    wb.save(filename = 'stockData.xlsx') #save the workbook
 
-def scores(tickerList):
+def predict(tickerList, inter):
     wb = op.load_workbook(filename = 'stockData.xlsx')
-    sh = wb['stockData']
+    sh = wb[inter]
     prediction = []
-
+    print('\n')
+    
     for i in tickerList:
         print(i)
-        
-        x = ls.getCurrent(i, 'daily')
+        print('(Getting Current Data from AlphaVantage)')
+        x = sd.getCurrent(i, inter)
         tm.sleep(60)
         
         nameFound = False
         
-        for cell in sh['A']:     
+        for cell in sh['A']:
             if(cell.value == i): #if the stock already exists
+                print('(Reading Data from Spreadsheet)')
                 m = np.array([sh['B' + str(cell.row)].value,
                      sh['C' + str(cell.row)].value,
                      sh['D' + str(cell.row)].value,
@@ -116,13 +84,15 @@ def scores(tickerList):
                 score = w*(m*x + b)
                 score = score.sum()
                 prediction.append(score)
+                print(prediction)
     allPred = pd.DataFrame({'ticker': tickerList, 'score': prediction})
     allPred.sort_values('score', inplace = True)
     return(allPred)
     
-tickerList = ['PLUG', 'NIO', 'RIDE', 'FUV', 'FCEL', 'CBAT', 'BLNK', 'SPY']
+tickerList = ['NKLA', 'MSFT', 'SNE', 'IBM'] #'NIO', 'TSLA', 'PLUG', 
+inter = 'daily'
 print('Estimated Time: %d minutes' %(5*np.size(tickerList)))
-learn(tickerList)
-tm.pause(60)
-ranking = scores(tickerList)
+# learn(tickerList, inter)
+# tm.sleep(60)
+ranking = predict(tickerList, inter)
 print(ranking)
